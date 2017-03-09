cdadf4702c54fe95e73a12b53b5795cc3aeda307
André Santos <andrewskad@gmail.com>
2017-03-09 18:09:28 +0000

    Change from Shared Project to Portable Class Library

--- demo-xamarin/demo_xamarin.Android/MainActivity.cs
+++ demo-xamarin/demo_xamarin.Android/MainActivity.cs
@@ -9,19 +9,19 @@ using Android.OS;
 
 namespace demo_xamarin.Droid
 {
-	[Activity (Label = "demo_xamarin", Icon = "@drawable/icon", Theme="@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
-	{
-		protected override void OnCreate (Bundle bundle)
-		{
-			TabLayoutResource = Resource.Layout.Tabbar;
-			ToolbarResource = Resource.Layout.Toolbar; 
+    [Activity(Label = "demo_xamarin", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
 
-			base.OnCreate (bundle);
+            base.OnCreate(bundle);
 
-			global::Xamarin.Forms.Forms.Init (this, bundle);
-			LoadApplication (new demo_xamarin.App ());
-		}
-	}
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
 }
 
